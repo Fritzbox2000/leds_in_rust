b03f48d1f722ec2010fd5cae928dfa9c2515621e
Woonters <woonters@gmail.com>
2023-06-05 19:43:55 +0100

    Testing a library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rs_ws281x
+
 fn main() {
-    println!("Hello, world!");
+    let controller = rs_ws281x::ControllerBuilder::new().freq(800_000).dma(10).channel(ChannelBuilder::new().pin(18).counter(10).strip_type(StripType::WS2811Rgb).brightness(255).build()).build();
+    let leds = controller.leds_mut(0);
+    leds[0] = [255,255,255,0];
+    controller.render();
 }
